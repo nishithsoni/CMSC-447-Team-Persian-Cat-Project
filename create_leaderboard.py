58c1c4d87ede93d436ef7494118a2e7e213a7af9
Nishith Soni <nsoni2@umbc.edu>
2024-04-27 17:49:27 -0400

    login and level picker final edits

--- create_leaderboard.py
+++ create_leaderboard.py
@@ -3,10 +3,19 @@ import sqlite3
 DATABASE = 'leaderboard.db'
 
 def create_leaderboard():
+    # delete leaderboard if it already exists
+    try:
+        conn = sqlite3.connect(DATABASE)
+        c = conn.cursor()
+        c.execute('''DROP TABLE leaderboard''')
+        conn.commit()
+        conn.close()
+    except:
+        pass
     conn = sqlite3.connect(DATABASE)
     print("Opened database successfully")
     c = conn.cursor()
-    c.execute('''CREATE TABLE leaderboard (username text, num_solved int)''')
+    c.execute('''CREATE TABLE leaderboard (username text, curr_level int, num_solved int)''')
     print("Table created successfully")
     conn.commit()
     conn.close()
